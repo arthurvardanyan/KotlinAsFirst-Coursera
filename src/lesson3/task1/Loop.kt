139d5b33b8cfc32faac037b7f9be0d99888d20c5
arthur week 1 <sdoxnute@gmail.com>
2020-03-31 19:39:19 +0300

    фибаничи цикл

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -74,7 +74,18 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int {
+    var result1 = 0
+    var result2 = 1
+    var result = 1
+    if (n == 1) return  result
+    else for (i in 2..n) {
+        result = result1 + result2
+        result1 = result2
+        result2 = result
+    }
+    return result
+}
 
 /**
  * Простая
